f1791c67b79f7683689961ce16a5867771aa42aa
Alexander <vip.alex2132@mail.ru>
2022-09-06 21:12:46 +0300

    Hallo program

--- Kushch.cpp
+++ Kushch.cpp
@@ -1,6 +1,13 @@
-
+#include <iostream>
 
 int main()
 {
+    char symbol;
+
+    std::cout << "Hello World!" << std::endl;
+
+    std::cout << "Enter some symbol please: ";
+    std::cin >> symbol;
+
     return 0;
 }
